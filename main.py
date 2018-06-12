748b70afae07f31c7a117b2363fcd5e9ea7d4a5e
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-12 14:38:18 -0400

    Make the required directory if not already present

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ gif_plots = glob.glob('*.gif')
 
 for plot in gif_plots:
       filename = plot
-      plot_path = os.path.join(os.getcwd(), filename)
+      file_path = os.path.join(os.getcwd(), filename)
       plot = plot.split('-')
       date_name = plot[0]
 
       # Convert date_name into datetime objects.
-      date_time_object = datetime.datetime.strptime(date_name, '%Y%m%d').date()
+      date_time_object = datetime.datetime.strptime(date_name,
+                                                    '%Y%m%d').date()
       year_month_day = str(date_time_object).split('-')
 
-      #year_folder_path  = os.path.join(os.getcwd(), year_month_day[0])
-      #month_folder_path = os.path.join(os.getcwd(), year_month_day[0], year_month_day[1])
-      day_folder_path   = os.path.join(os.getcwd(), year_month_day[0], year_month_day[1], year_month_day[2])
-      #shutil.move()
-      print(plot_path)
+      day_folder_path   = os.path.join(os.getcwd(), year_month_day[0],
+                                 year_month_day[1], year_month_day[2])
+
+      if(not os.path.exists(day_folder_path)):
+            os.makedirs(day_folder_path)
 
       
