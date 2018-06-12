9bbd254003fb0988f3b57f7b8f23a086eaaf20ca
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-12 14:00:04 -0400

    Test path creatiion for folders for year, month and day

--- main.py
+++ main.py
@@ -19,4 +19,8 @@ for plot in gif_plots:
       # Convert date_name into datetime objects.
       date_time_object = datetime.datetime.strptime(date_name, '%Y%m%d').date()
       year_month_day = str(date_time_object).split('-')
-      
+
+      year_folder_path  = os.path.join(os.getcwd(), year_month_day[0])
+      month_folder_path = os.path.join(os.getcwd(), year_month_day[0], year_month_day[1])
+      day_folder_path   = os.path.join(os.getcwd(), year_month_day[0], year_month_day[1], year_month_day[2])
+      print(day_folder_path)
