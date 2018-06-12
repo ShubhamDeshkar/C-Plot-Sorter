8e7da72e8ff2483a629b75a6994885384073487b
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-12 13:41:16 -0400

    Create and print only date object from the date_name

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 import glob
 import shutil
 import datetime
-from datetime import datetime
 
 
 # It is assumed that this code file resides just outside the test-data
 # folder.
+
 test_data_path = os.path.join(os.getcwd(), 'test-data')
 os.chdir(test_data_path)
 gif_plots = glob.glob('*.gif')
@@ -15,4 +15,7 @@ for plot in gif_plots:
       filename = plot
       plot = plot.split('-')
       date_name = plot[0]
-      print(date_name)
+
+      # Convert date_name into datetime objects.
+      date_time_object = datetime.datetime.strptime(date_name, '%Y%m%d').date()
+      print(date_time_object)
