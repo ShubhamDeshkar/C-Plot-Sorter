026e6a2d17cc5d489df72aad4a3b36f335920d45
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-12 13:10:02 -0400

    Get date_name from the name of the plot

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 import glob
-import datetime
 import shutil
+import datetime
+from datetime import datetime
 
 
 # It is assumed that this code file resides just outside the test-data
 # folder.
 test_data_path = os.path.join(os.getcwd(), 'test-data')
 os.chdir(test_data_path)
-print(glob.glob('*.gif'))
+gif_plots = glob.glob('*.gif')
 
-
+for plot in gif_plots:
+      filename = plot
+      plot = plot.split('-')
+      date_name = plot[0]
+      print(date_name)
