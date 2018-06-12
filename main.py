f15c30b1634fd7c0bca0dc408d46cb9bafb8f2a6
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-12 10:30:20 -0400

    Print list of files ending in .gif

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import os
 import glob
 import datetime
 import shutil
+
+
+# It is assumed that this code file resides just outside the test-data
+# folder.
+test_data_path = os.path.join(os.getcwd(), 'test-data')
+os.chdir(test_data_path)
+print(glob.glob('*.gif'))
+
+
