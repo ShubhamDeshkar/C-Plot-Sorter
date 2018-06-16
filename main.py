fd366f6cfd77b9b8e158ecaac465a7123b5dbf06
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-16 09:36:53 -0400

    Add condintion in main for pattern matching

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import re
 # It is assumed that this code file resides just outside the test-data
 # folder.
 
-def ismatch(searchstring):
+def ismatch(filename):
+      # match the pattern for 8 digit followed by a '-' followed by 6 digits
       regex = re.compile('\d\d\d\d\d\d\d\d-\d\d\d\d\d\d')
-      if(regex.search(searchstrting)):
+
+      # if filename matches the pattern return True
+      if(regex.search(filename)):
             return 'True'
       else:
             return 'False'
@@ -26,19 +29,20 @@ if __name__ == '__main__':
             filename = plot
             file_path = os.path.join(os.getcwd(), filename)
             if(os.path.isfile(file_path)):
-                  plot = plot.split('-')
-                  date_name = plot[0]
+                  if(ismatch(filename)):
+                        plot = plot.split('-')
+                        date_name = plot[0]
 
-                  # Convert date_name into datetime objects.
-                  date_time_object = datetime.datetime.strptime(date_name,
-                                                                '%Y%m%d').date()
-                  year_month_day = str(date_time_object).split('-')
+                        # Convert date_name into datetime objects.
+                        date_time_object = datetime.datetime.strptime(date_name,
+                                                                      '%Y%m%d').date()
+                        year_month_day = str(date_time_object).split('-')
 
-                  day_folder_path   = os.path.join(os.getcwd(), year_month_day[0],
-                                             year_month_day[1], year_month_day[2])
+                        day_folder_path   = os.path.join(os.getcwd(), year_month_day[0],
+                                                   year_month_day[1], year_month_day[2])
 
-                  if(not os.path.exists(day_folder_path)):
-                        os.makedirs(day_folder_path)
+                        if(not os.path.exists(day_folder_path)):
+                              os.makedirs(day_folder_path)
 
-                  # Moves the file into its proper directory
-                  shutil.move(file_path, day_folder_path)
+                        # Moves the file into its proper directory
+                        shutil.move(file_path, day_folder_path)
