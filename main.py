05cb1de128845267d3d09756b62f4d606f0364bd
Shubham Deshkar <shubham_deshkar@yahoo.com>
2018-06-16 09:45:40 -0400

    Update main.py with comments

--- main.py
+++ main.py
@@ -21,28 +21,39 @@ def ismatch(filename):
 
 
 if __name__ == '__main__':
+      # get the test-data folder path
       test_data_path = os.path.join(os.getcwd(), 'test-data')
       os.chdir(test_data_path)
+
+      # list all the files and folders inside test-data path
       gif_plots = os.listdir(test_data_path)
 
       for plot in gif_plots:
             filename = plot
+            # create a path for the filename
             file_path = os.path.join(os.getcwd(), filename)
+            # if file path is a file, proceed
             if(os.path.isfile(file_path)):
+                  # check if the filename matches the pattern
                   if(ismatch(filename)):
+                        # split about '-'
                         plot = plot.split('-')
+                        # save the first element (index=0)
                         date_name = plot[0]
 
                         # Convert date_name into datetime objects.
                         date_time_object = datetime.datetime.strptime(date_name,
                                                                       '%Y%m%d').date()
+                        # split about '-' ['yyyy', 'mm', 'dd']
                         year_month_day = str(date_time_object).split('-')
 
+                        # create a folder path using above list elements
                         day_folder_path   = os.path.join(os.getcwd(), year_month_day[0],
                                                    year_month_day[1], year_month_day[2])
 
+                        # create the folder if not present already
                         if(not os.path.exists(day_folder_path)):
                               os.makedirs(day_folder_path)
 
-                        # Moves the file into its proper directory
+                        # Move the file into its proper directory
                         shutil.move(file_path, day_folder_path)
